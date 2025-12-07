0309ab0547d3f13deb2556bab863b3c2c33e7d64
Rosianu Calin <rosianu.calin@gmail.com>
2025-12-07 10:49:13 +0200

    finished part2 of the day1 challange

--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 const char *DIAL_SEQUENCE_EXAMPLE =
-    "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL999\nR14\nL82";
+    "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
 
 #ifdef DAY1_EXAMPLE
     #define DO_EXAMPLE true
@@ -51,28 +51,42 @@ std::vector<DialSequence> processSequence(std::string inputSequence) {
   return sequences;
 }
 
-int applySequence(int currentPos, DialSequence &sequence) {
+int applySequence(int currentPos, DialSequence &sequence, int &clicks) {
   int displacement =
       (sequence.value < 100) ? sequence.value : sequence.value % 100;
+  clicks += ((sequence.value < 100)
+                 ? 0
+                 : static_cast<int>(floor(sequence.value / 100.0)));
   int newPos = currentPos;
   switch (sequence.direction) {
   case LEFT:
     if (currentPos > displacement)
       newPos = currentPos - displacement;
-    else
+    else {
       newPos = 100 - (displacement - currentPos);
-    if (newPos >= 100)
+      /* std::println("currentPos: {}| displacement: {} | newPos: {} | Add one click to {}",
+                   currentPos, sequence.value, newPos, clicks); */
+      if (currentPos != 0)
+        clicks += 1;
+    }
+    if (newPos >= 100) {
+      /* std::println("currentPos: {}| displacement: {} | newPos: {} |",
+                   currentPos, sequence.value, newPos); */
       newPos = newPos - 100;
+    }
     break;
   case RIGHT:
     newPos = currentPos + displacement;
-    if (newPos >= 100)
+    if (newPos >= 100) {
       newPos = newPos - 100;
+      clicks += 1;
+    }
     break;
   default:
     std::println("Something Went Horribly Wrong if you are here");
     break;
   }
+  //std::println("Clicks: {}", clicks);
 
   return newPos;
 }
@@ -81,10 +95,11 @@ int main() {
   int starting_position = 50;
   int currentPosition = starting_position;
   int counter = 0;
+  int clicks = 0;
   auto seqs = processSequence((DO_EXAMPLE) ? std::string(DIAL_SEQUENCE_EXAMPLE)
                                            : readFile("day1_input.txt"));
   for (auto seq : seqs) {
-    currentPosition = applySequence(starting_position, seq);
+    currentPosition = applySequence(starting_position, seq, clicks);
     if (currentPosition == 0) {
       counter++;
     }
@@ -92,6 +107,7 @@ int main() {
   }
 
   std::println("Result: {}", counter);
+  std::println("Number of times the dial passes through 0: {}", clicks);
 
   return 0;
 }
